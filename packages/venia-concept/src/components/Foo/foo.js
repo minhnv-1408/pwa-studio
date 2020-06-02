07d8430477fce8fb804f1e54a475228cde6cadc5
FRAMGIA\nguyen.van.minhb <nguyen.van.minhb@sun-asterisk.com>
2020-06-02 09:10:58 +0700

    feat: Add a static route

---
+++ packages/venia-concept/src/components/Foo/foo.js
@@ -0,0 +1,10 @@
+import React, { useEffect } from 'react';
+
+const Foo = () => {
+
+  return (
+    <h1>Hello World JSX</h1>
+  );
+}
+
+export default Foo;
---
+++ packages/venia-concept/src/components/Routes/routes.js
@@ -0,0 +1,44 @@
+import React, { Suspense, lazy } from 'react';
+import { Route, Switch, useLocation } from 'react-router-dom';
+
+import { fullPageLoadingIndicator } from '@magento/venia-ui/lib/components/LoadingIndicator';
+import MagentoRoute from '@magento/venia-ui/lib/components/MagentoRoute';
+import { useScrollTopOnChange } from '@magento/peregrine/lib/hooks/useScrollTopOnChange';
+
+const CartPage = lazy(() => import('@magento/venia-ui/lib/components/CartPage'));
+const CheckoutPage = lazy(() => import('@magento/venia-ui/lib/components/CheckoutPage'));
+const CreateAccountPage = lazy(() => import('@magento/venia-ui/lib/components/CreateAccountPage'));
+const Search = lazy(() => import('@magento/venia-ui/lib/RootComponents/Search'));
+const Foo = lazy(() => import('../Foo'));
+
+const Routes = () => {
+    const { pathname } = useLocation();
+    useScrollTopOnChange(pathname);
+
+    return (
+        <Suspense fallback={fullPageLoadingIndicator}>
+            <Switch>
+                <Route exact path="/foo">
+                    <Foo />
+                </Route>
+                <Route exact path="/search.html">
+                    <Search />
+                </Route>
+                <Route exact path="/create-account">
+                    <CreateAccountPage />
+                </Route>
+                <Route exact path="/cart">
+                    <CartPage />
+                </Route>
+                <Route exact path="/checkout">
+                    <CheckoutPage />
+                </Route>
+                <Route>
+                    <MagentoRoute />
+                </Route>
+            </Switch>
+        </Suspense>
+    );
+};
+
+export default Routes;
